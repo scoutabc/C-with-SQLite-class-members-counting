c64191bea34a32f44650916137dcd79e01e28b3e
scoutabc <scout_169@163.com>
2022-12-17 15:13:00 +0800

    complete student information insert

--- create.c
+++ create.c
@@ -4,27 +4,8 @@
 #include<stdlib.h>
 #include<locale.h>
 #include<string.h>
-#include<iconv.h>//When we gcc -o this file,we must plus -liconv.
 #include"create.h"
 //This is a part of this project, it can create class and student information to tables.
-
-//We will use this function to turn gbk string to utf8 string.
-int gbk_to_utf8(char *inbuf,int inlen,char *outbuf,int outlen) {
-    iconv_t cd; //That is conversion codes
-    int rc;
-    char **pin = &inbuf;
-    char **pout = &outbuf;
-    cd = iconv_open("UTF-8","GB2312");
-    if (cd == 0) {
-        return -1;
-    }
-    memset(outbuf,0,outlen);
-    if (iconv(cd,pin,&inlen,pout,&outlen) == -1) {
-        return -1;
-    }
-    iconv_close(cd);
-    return 0;
-}
 
 //We will use this function to do some sql queries
 void do_query(char *query,sqlite3 *db) {
@@ -38,7 +19,7 @@ void do_query(char *query,sqlite3 *db) {
     }
 }
 
-void create_message(sqlite3 *db) {
+void create_message(sqlite3 *db,sqlite3_stmt *stmt) {
     setlocale(LC_ALL,"");
     //I create five tables in that database
     char query[1000] = "CREATE TABLE IF NOT EXISTS Students(" \
@@ -80,43 +61,52 @@ void create_message(sqlite3 *db) {
     wchar_t school[30];
     wchar_t class[30];
     do {
-        wcscpy(question,L"请输入班主任姓名：");
+        wcscpy(question,L"请输入班主任姓名:");
         printf("%ls",question);
         scanf("%ls",name);
-        wcscpy(question,L"请输入班级号码：");
+        wcscpy(question,L"请输入班级号码:");
         printf("%ls",question);
         scanf("%d",&num);
-        wcscpy(question,L"请输入学校及班级名称：");
+        wcscpy(question,L"请输入学校及班级名称:");
         printf("%ls",question);
         scanf("%ls%ls",school,class);
-        snwprintf(question,150,L"班级号码：%d，班级名称：%ls，班主任名称：%ls，学校名称：%ls",num,class,name,school);
+        snwprintf(question,150,L"班级号码：%d,班级名称:%ls,班主任名称:%ls,学校名称:%ls",num,class,name,school);
         printf("%ls\n",question);
-        wcscpy(question,L"您确定保存？（保存输入1，否则输入0）");
+        wcscpy(question,L"您确定保存?(保存输入1,否则输入0)");
         printf("%ls",question);
         scanf("%d",&saving);
     }while(saving == 0);
-    char utf8_query[1000];
-    sprintf(query,"INSERT INTO Class VALUES(%d,'%ls','%ls','%ls');",num,class,name,school);
-    gbk_to_utf8(query,1000,utf8_query,1000);
-    do_query(utf8_query,db);
-    wcscpy(question,L"您班有几名同学？");
+    sprintf(query,"INSERT INTO Class VALUES(?,?,?,?);");
+    sqlite3_prepare_v2(db,query,-1,&stmt,NULL);
+    sqlite3_bind_int(stmt,1,num);
+    sqlite3_bind_text16(stmt,2,class,-1,SQLITE_TRANSIENT);
+    sqlite3_bind_text16(stmt,3,name,-1,SQLITE_TRANSIENT);
+    sqlite3_bind_text16(stmt,4,school,-1,SQLITE_TRANSIENT);
+    sqlite3_step(stmt);
+    sqlite3_finalize(stmt);
+    wchar_t student_name[20];
+    wcscpy(question,L"您班有几名同学?");
     printf("%ls",question);
     int number_of_people;
-    wchar_t student_name[20];
-    scanf("%d",number_of_people);
+    scanf("%d",&number_of_people);
     for (int i = 1; i <= number_of_people; i++) {
         printf("%d:",i);
         scanf("%ls",student_name);
-        wcscpy(question,L"确定保存？（保存输入1，否则输入其他任意字符）");
-        while (getchar() != '\n');
+        wcscpy(question,L"确定保存?(保存输入1,否则输入其他任意字符)");
         printf("%ls",question);
         char saving;
+        while (getchar() != '\n');
         scanf("%c",&saving);
-        if (saving != 1){
+        if (saving != '1'){
+            i = i - 1;
             continue;
         }
-        sprintf(query,"INSERT INTO Students VALUES(%d,'%ls',%d);",i,student_name,num);
-        gbk_to_utf8(query,1000,utf8_query,1000);
-        do_query(utf8_query,db);
+        sprintf(query,"INSERT INTO Students VALUES(?,?,?);");
+        sqlite3_prepare_v2(db,query,-1,&stmt,NULL);
+        sqlite3_bind_int(stmt,1,i);
+        sqlite3_bind_text16(stmt,2,student_name,-1,SQLITE_TRANSIENT);
+        sqlite3_bind_int(stmt,3,num);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
     }
 }
